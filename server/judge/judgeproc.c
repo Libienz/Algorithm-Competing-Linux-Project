52ee00c230ec26d835bcce67dd65a88d47d8c9e7
Libienz <Leekh10195@gmail.com>
2022-12-04 20:15:53 +0900

    pipe judge done

--- server/judge/judgeproc.c
+++ server/judge/judgeproc.c
@@ -4,142 +4,134 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
 
 #define BUFSIZE 257
 
-
 /*
-   getusrans -> get user answer
-   입력 사례 한개에 대해서 유저의 정답을 output.txt라는 파일에 담는다.
-	1. fork를 통해서 자식 프로세스가 인자로 받은 유저 제출물을 실행
-	2. 입출력 redirection을 통해서 유저 제출 프로그램은 inject.txt를 통해 입력을 받게 됨
-	3. 후에 정답인지 판별하게 될 유저의 출력은 output.txt에 담기게 된다.
-   즉 getusrans 함수가 실행되는 시점에는 inject.txt가 사례 하나로 채워져 있어야 함
+   judgeproc
+   명령행으로 받은 프로그램이 특정 알고리즘 문제에 대해 
+   정답인지 확인하는 채점 프로세스
+   입출력 모범사례가 들어있는 답지를 대입해 봄으로써 정답인지 확인한다.
+   argv[1]: execfn (실행파일명)
+   argv[2]: solfn  (답지)
+   사용하기 위한 명령은 다음과 같다
+   ./judgeproc [progname] [sol_file_name]
 */
-int getusrans(char* execfn) {
-    
-    int fd[2];
-    int ifd, ofd;
-    pid_t pid;
-    char buf[BUFSIZE];
-    int len, status;
-
-    
-    ifd = open("./temp/inject.txt",O_RDONLY | O_CREAT, 0644);
-    ofd = open("./temp/output.txt",O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (ifd == -1) {
-	perror("open");
-	exit(1);
-    }
-    if (ofd == -1) {
-	perror("open");
-	exit(1);
-    }
-
-
-    switch (pid= fork()) {
-	//fork error
-	case -1 :
-	    perror("fork");
-	    exit(1);
-	    break;
-	//child proc: 입출력 redirection하고 제출물을 실행시켜 본다.
-	case 0:
-
-	    if(ifd != 0) {
-		dup2(ifd, 0);
-		close(ifd);
-	    }
-
-	    if (ofd != 1) {
-		dup2(ofd,1);
-		close(ofd);
-	    }
-	    //유저 제출물 실행
-	    char *argv[1];
-	    argv[0] = NULL;
-	    if (execv(execfn,argv) == -1) {
-		perror("execlp");
-		exit(1);
-	    }
-	    exit(1);
-	    break;
-	//parent proc: 부모는 자식이 실행을 마칠 때 까지 기다림
-	default:
-	    waitpid(pid, &status, 0);
-	    break;
-	    
-    }
-}
-
-/*
-   main()에 대한 설명
-   argv[1]: execfn (유저가 제출한 실행파일명)
-   argv[2]: solfn (서버가 보관하고 있는 답안 파일명)
-   유저 제출물을 1번 답지와 비교해서 채점하고 싶다면 요청은 다음과 같다
-   ./judgeproc usrprog ./answers/1.sol
-*/
-
 int main(int argc, char* argv[]) {
 
-    FILE *fp, *ifp, *ofp;
+    FILE* fp;
+    pid_t pid;
+    int status, len;
     char buf[BUFSIZE];
-    int c;
     char model_input[BUFSIZE];
     char model_output[BUFSIZE];
-    char usr_answer[BUFSIZE];
-    
+    int fd1[2], fd2[2];
+
+    //답지 파일 오픈 
+    //답지는 5가지 입력 케이스와 각 입력에 대한 모범출력 답안을 가지고 있다.
     if ((fp = fopen(argv[2],"r")) == NULL) {
 	perror("fopen");
 	exit(1);
     }
 
-    //유저 프로그램이 5가지 입력 예시에 대해서 모범답안을 출력하는지 확인해야 함
-    //답지를 5번 반복해서 읽어와서 각 입력예시마다 정답인지 확인한다.
-    while ((fgets(buf,BUFSIZE,fp)) != NULL) {
+    //답지 읽어오기 
+    while (fgets(buf,BUFSIZE,fp) != NULL) {
 	
-	if ((ifp = fopen("./temp/inject.txt","w")) == NULL) {
-	    perror("fopen");
+	//유저 프로그램이 실행되었을 때 
+	//입력을 넘겨주고 출력을 받아올 파이프
+	//양방향으로 설정
+	if (pipe(fd1) == -1) {
+	    perror("pipe");
 	    exit(1);
 	}
 
-	//userProg의 input으로 주입할 inject.txt 준비
+	if (pipe(fd2) == -1) {
+	    perror("pipe");
+	    exit(1);
+	}
+
+	//답지는 입력과 입력에 대한 정답을 ':'으로 구분 strtok을 이용해서 
+	//한가지 사례 입력과 입력에 대한 모범답안을 받는다.
 	strcpy(model_input, strtok(buf,":"));
-	strcat(model_input, "\n");
-	printf("사례 입력: %s\n", model_input);
-	fputs(model_input, ifp);
-	fclose(ifp);
-	
-	//모범 답안 준비. 
-	//후에 유저 답안과 비교할 예정
-	strcpy(model_output, strtok(NULL,":"));
+	strcpy(model_output, strtok(NULL,":"));	
 
-	//유저의 답안을 output.txt에 담는다.
-	getusrans(argv[1]);
+	//파이프에 입력을 미리 넣어 놓는다.
+	//후에 자식 프로세스가 입출력 redirection 설정을 완료하고
+        //채점할 프로그램을 실행하면 자연스럽게 입력으로 넘어갈 것 
+	write(fd1[1], model_input,(int) strlen(model_input)+1);
 
+	switch (pid= fork()) {
+	    //fork error
+	    case -1 :
+		perror("fork");
+		exit(1);
+		break;
+	    //child proc
+	    case 0:
+		//파이프 방향 설정
+		close(fd1[1]);
+		close(fd2[0]);
 
-	if ((ofp = fopen("./temp/output.txt","r")) == NULL) {
-	    perror("fopen");
-	    exit(1);
+		//자식 프로세스는 표준 입력을 fd[0]이 가리키는 파이프에서 읽는다
+		if(fd1[0] != 0) {
+		    dup2(fd1[0], 0);
+		    close(fd1[0]);
+		}
+		//자식 프로세스는 자신의 표준 출력을 fd[2]가 가리키는 파이프로 보낸다
+		if(fd2[1] != 1) {
+		    dup2(fd2[1], 1);
+		    close(fd2[1]);
+		}
+
+		//유저 제출물 실행
+		//자식 프로세스가 무언가 출력했다면 해당 출력은 
+		//부모 프로세스로 향하는 파이프에 있을 것임
+		char *args[1];
+		args[0] = NULL;
+		if (execv(argv[1], args) == -1) {
+		    perror("execv");
+		    exit(1);
+		}
+ 
+		exit(1);
+
+		//각각의 입출력 사례에 대해 파이프를 매번 새로 연결해야 함으로 (새로 실행한 프로세스에게 연결해야 하기 때문) 파이프 닫기
+		close(fd1[0]);
+		close(fd2[1]);
+		break;
+
+	    //parent proc
+	    default:
+		//자식이 종료할 때 까지 wait
+		waitpid(pid, &status, 0);
+		close(fd1[0]);
+		close(fd2[1]);
+		
+		//입력에 대해서 프로그램이 어떤 출력을 내었는지 파이프에서 꺼내본다.
+		len = read(fd2[0],buf,256);
+		if (len == -1) {
+		    perror("read");
+		    exit(1);
+		}
+		buf[len] = '\0';
+
+		printf("buf: %s\n",buf);
+		printf("model: %s\n",model_output);
+
+		//특정 입력을 주었을 때 자식의 답이 모범출력과 다르다면 오답
+		if (strcmp(buf,model_output) != 0) {
+		    printf("오답\n");
+		    return -1;
+		}
+		//파이프 닫아주기
+		close(fd1[1]);
+		close(fd2[0]);
+		break;
 	}
-
-	//output.txt에서 usr 프로그램이 출력한 string 가져온다.
-	fgets(usr_answer,BUFSIZE,ofp);
-	printf("usr 정답: %s\n", usr_answer);
-	printf("모범 정답: %s\n", model_output);
-	
-	//usr 정답과 모범 답안을 비교 다르면 오답!
-	if(strcmp(usr_answer, model_output)!=0) { 
-	    printf("오답\n");
-	    return -1;
-	}
-
-	fclose(ofp);
     }
-    //위의 반복문동안 모두 정답처리되어서 반복문을 탈출했다면 정답
-    fclose(fp);
+
+    //모든 case에 대해 모범출력을 보였다면 정답
     printf("정답\n");
-    return 0;
+    return 1;
 
 }
--- server/judge/maybereuse/test.c
+++ server/judge/maybereuse/test.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-int main(){
-    
+
+int main(void) {
 
     int num1, num2;
+
     scanf("%d %d", &num1, &num2);
-    printf("%d\n",num1+ num2);
+    printf("%d\n", num1 + num2);
 
 }
-
-
-  
